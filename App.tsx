3c5434e45e184335d107e5e0a53057b923b7c1dc
Gonçalo Silva <goncalompjsilva@gmail.com>
2025-07-15 13:57:32 +0100

    Refactor project structure: remove old files, add TypeScript support, and implement Home screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './app/screens/Home';
 
-export default function App() {
+export default function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
---
+++ app/screens/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>Welcome to the Home Screen!</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
